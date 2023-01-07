f6bedff4ff7c1f194a9971b7a02d99398d55fa2d
DonMGameR <donmoutsystems.js@gmail.com>
2023-01-07 17:01:59 -0300

    Criando posições randômicas

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,3 +9,21 @@ function ajustaTamanhoPalcoJogo() {
 }
 
 ajustaTamanhoPalcoJogo()
+
+var posicaoX = Math.floor(Math.random() * largura) - 90;
+var posicaoY = Math.floor(Math.random() * altura) - 90;
+
+posicaoX = posicaoX < 0 ? 0 : posicaoX;
+posicaoY = posicaoY < 0 ? 0 : posicaoY;
+
+console.log(posicaoX, posicaoY);
+
+// cria o elemento html
+var mosca = document.createElement('img');
+mosca.src = '/assets/img/mosca.png';
+mosca.className = 'mosca';
+mosca.style.left = posicaoX + 'px';
+mosca.style.top = posicaoY + 'px';
+mosca.style.position = 'absolute';
+
+document.body.appendChild(mosca);
